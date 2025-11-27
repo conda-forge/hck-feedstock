fed1719ceb2500b0e60bb3fa8e391ae57c622eea
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-26 17:13:05 -0800

    use rattler

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,10 +1,13 @@
+@echo on
+@setlocal EnableDelayedExpansion
+
 :: Check that all downstream libraries licenses are present
-cargo-bundle-licenses --format yaml --output CI.THIRDPARTY.yml
+cargo-bundle-licenses --format yaml --output CI.THIRDPARTY.yml || goto :error
 
 :: build
 cargo install --bins --no-track --locked --root "%PREFIX%" --path . || goto :error
 
-goto :EOF
+goto :eof
 
 :error
 echo Failed with error #%errorlevel%.
